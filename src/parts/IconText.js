37f521a47801a00a75a880c0e839e9d7101e7a2b
ian lombu <citrakemurnian@gmail.com>
2021-02-08 10:47:25 +0700

    Header Component

--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -1,13 +1,12 @@
-import Button from 'elements/Button'
-import React from 'react'
-import Button from 'elements/Button'
+import React from "react";
+import Button from "elements/Button";
 
 const IconText = () => {
-    return (
-        <Button className="brand-text-icon" href="" type="link">
-            Stay<span className="text-gray-900">cation.</span>
-        </Button>
-    )
-}
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      Stay<span className="text-gray-900">cation.</span>
+    </Button>
+  );
+};
 
-export default IconText
+export default IconText;
