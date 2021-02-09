ea2288157c680919a63f9a8e2419b941ca3806f4
ian lombu <citrakemurnian@gmail.com>
2021-02-09 13:34:19 +0700

    Star Component

---
+++ src/parts/Testimony.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Star from "elements/Star";
+
+const Testimony = () => {
+  return (
+    <div>
+      <Star value={2.75} width={35} height={35} spacing={4} />
+    </div>
+  );
+};
+
+export default Testimony;
